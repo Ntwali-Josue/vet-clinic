868e03e9b190ccf5051b67bf90be14afa70670ed
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-11 22:21:05 +0200

    Add few other queries for deletion

--- queries.sql
+++ queries.sql
@@ -4,3 +4,8 @@ UPDATE animals SET species='unspecified';
 UPDATE animals SET species='';
 UPDATE animals SET species='digimon' WHERE name like '%mon';
 UPDATE animals SET species='pokemon' WHERE species ='';
+
+DELETE FROM animals;
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+UPDATE animals SET weight_kg =  weight_kg * -1;
+UPDATE animals SET weight_kg =  weight_kg * -1 where weight_kg < 0;
